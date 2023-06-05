ec1747eaf73ae0fe077fc84fbbc6dab069305fbe
Chetan <chetan.kvvk@gmail.com>
2023-06-05 13:32:44 +0530

    better appraoch

--- 1-Set-Matrix-Zeroes.cpp
+++ 1-Set-Matrix-Zeroes.cpp
@@ -4,6 +4,7 @@
 
 using namespace std;
 
+// O(m+n) space solution
 void setZeros(vector<vector<int>>& matrix) {
     vector<bool> setRowZero(matrix.size(), false);
     vector<bool> setColumnZero(matrix[0].size(), false);
@@ -30,3 +31,50 @@ void setZeros(vector<vector<int>>& matrix) {
         }
     }
 }
+
+// O(1) space solution
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+
+        int row = matrix.size();
+        int column = matrix[0].size();
+
+        bool firstZero = false;
+
+        for (int i=0; i<row; i++) {
+        
+            if (matrix[i][0] == 0) {
+                firstZero = true;
+            }
+
+            for (int j=1; j < column; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[0][j] = 0;
+                    matrix[i][0] = 0;
+                }
+            }
+
+        }
+
+        for (int i=1; i < row; i++) {
+            for (int j=1; j<column; j++) {
+                if (matrix[i][0] == 0 or matrix[0][j] == 0) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        if (matrix[0][0] == 0) {
+            for (int i=0; i < column; i++) {
+                matrix[0][i] = 0;
+            }
+        }
+
+        if (firstZero) {
+            for (int i=0; i < row; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+    }
+};
